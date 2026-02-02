9687363cb02c5f32377846ae8bb641796e9b253a
bokargiu <erickjstaack07@gmail.com>
2026-02-02 17:07:22 -0300

    Footer feito

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider'
+import { Button } from "primeng/button";
+import { AppRoutingModule } from "src/app/app-routing.module";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    Button,
+],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
