b9a16b5ce718212f2e719bc21017996d5744c37d
bokargiu <erickjstaack07@gmail.com>
2026-02-03 12:32:17 -0300

    Footer foi melhorado (diminuido) e os inputs do login foram criados mas sem lógica

--- src/app/Pages/login-page/login-page.component.ts
+++ src/app/Pages/login-page/login-page.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "src/app/Components/header/header.component";
 import { FooterComponent } from "src/app/Components/footer/footer.component";
+import { CardModule } from "primeng/card";
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { Button } from "primeng/button";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    CardModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputTextModule,
+    CommonModule,
+    FormsModule,
+    FloatLabelModule,
+    PasswordModule,
+    Button
+],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
-
+  user: string | undefined
+  password: string | undefined
 }
