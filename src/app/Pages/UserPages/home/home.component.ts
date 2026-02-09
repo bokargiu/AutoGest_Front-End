e85c7787cbd7d1023cec5c678dc1b1e61b0074fd
bokargiu <erickjstaack07@gmail.com>
2026-02-09 20:11:40 -0300

    UserPage criado

--- src/app/Pages/UserPages/home/home.component.ts
+++ src/app/Pages/UserPages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MenuModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
