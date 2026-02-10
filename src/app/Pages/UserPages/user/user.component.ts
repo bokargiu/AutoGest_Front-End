f03b67a773ba9373a5f9a601e3b5bbd2aa400294
bokargiu <erickjstaack07@gmail.com>
2026-02-10 20:55:37 -0300

    UserSideMenu e HeaderUser foram criados e integrados no UserPage

--- src/app/Pages/UserPages/user/user.component.ts
+++ src/app/Pages/UserPages/user/user.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideMenuComponent } from "src/app/Components/UserPageComponents/side-menu/side-menu.component";
+import { UserHeaderComponent } from "src/app/Components/UserPageComponents/user-header/user-header.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [
-    RouterOutlet
-  ],
+    RouterOutlet,
+    SideMenuComponent,
+    UserHeaderComponent
+],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
