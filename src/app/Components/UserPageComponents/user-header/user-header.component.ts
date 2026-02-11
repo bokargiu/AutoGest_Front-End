9567086d03e1c5088b58631689e460a67a236bb0
bokargiu <erickjstaack07@gmail.com>
2026-02-10 21:30:38 -0300

    UserHeader como botão logout funcional

--- src/app/Components/UserPageComponents/user-header/user-header.component.ts
+++ src/app/Components/UserPageComponents/user-header/user-header.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
+import { Ripple } from "primeng/ripple";
+import { ButtonDirective } from "primeng/button";
+import { AuthService } from 'src/app/Services/AuthService/auth.service';
 
 @Component({
   selector: 'app-user-header',
   standalone: true,
-  imports: [],
+  imports: [
+    AvatarModule,
+    Ripple,
+    ButtonDirective
+],
   templateUrl: './user-header.component.html',
   styleUrl: './user-header.component.css'
 })
 export class UserHeaderComponent {
-
+  constructor(private _auth:AuthService){}
+  logout(){
+    this._auth.Logout();
+  }
 }
