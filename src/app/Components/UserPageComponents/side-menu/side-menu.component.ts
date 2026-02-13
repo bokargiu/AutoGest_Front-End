e9f34f9d2e2615d4a696904d84471be4043dfc12
bokargiu <erickjstaack07@gmail.com>
2026-02-12 21:09:12 -0300

    Botões do UserSideBar teóricamente funcional

--- src/app/Components/UserPageComponents/side-menu/side-menu.component.ts
+++ src/app/Components/UserPageComponents/side-menu/side-menu.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Button } from "primeng/button";
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
@@ -15,36 +15,52 @@ import { RouterModule } from '@angular/router';
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent implements OnInit {
-  itens: { label:string; icon:string; router:string }[] = [];
+  itens: { label:string; icon:string; router:string; class:string}[] = [];
+
+  constructor(private _router:Router) {}
 
   ngOnInit(): void {
       this.itens = [
       {
         label: 'Home',
         icon: 'pi pi-home',
-        router: '/home'
+        router: './home',
+        class: this.isRoute('/user/home')
       },
       {
         label: 'Agenda',
         icon: 'pi pi-calendar',
-        router: '/agenda'
+        router: './agenda',
+        class: this.isRoute('/user/agenda')
       },
       {
         label: 'Clientes',
         icon: 'pi pi-users',
-        router: '/clientes'
+        router: './clientes',
+        class: this.isRoute('/user/clientes')
       },
       {
         label: 'Serviços',
         icon: 'pi pi-briefcase',
-        router: '/servicos'
+        router: './servicos',
+        class: this.isRoute('/user/servicos')
       },
       {
         label: 'Dashboard',
         icon: 'pi pi-objects-column',
-        router: '/dashboard'
+        router: './dashboard',
+        class: this.isRoute('/user/dashboard')
       }
     ]
+  }
+
+  private isRoute(router:string){
+    if(router === this._router.url){
+      return 'secondary-btn'
+    }
+    else{
+      return 'primary-btn'
+    }
   }
   
 }
