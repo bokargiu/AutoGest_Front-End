4e0b94221b807f09760b577c66c00782274d9f1a
bokargiu <erickjstaack07@gmail.com>
2026-02-13 20:21:14 -0300

    toolbar de options do UserClients está parcialmente feita

--- src/app/Pages/UserPages/user-clients/user-clients.component.ts
+++ src/app/Pages/UserPages/user-clients/user-clients.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+import { AvatarModule } from "primeng/avatar";
+import { Ripple } from "primeng/ripple";
+import { InputTextModule } from "primeng/inputtext";
+import { InputIconModule } from 'primeng/inputicon';
+import { IconFieldModule } from 'primeng/iconfield';
 
 @Component({
   selector: 'app-user-clients',
   standalone: true,
-  imports: [],
+  imports: [
+    ToolbarModule,
+    ButtonModule,
+    AvatarModule,
+    Ripple,
+    InputTextModule,
+    InputIconModule,
+    IconFieldModule
+],
   templateUrl: './user-clients.component.html',
   styleUrl: './user-clients.component.css'
 })
