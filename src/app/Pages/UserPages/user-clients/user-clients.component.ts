20071e25fbd0eed2505c7f010ee21e40dc9e4dfb
bokargiu <erickjstaack07@gmail.com>
2026-02-18 13:25:30 -0300

    tabela de clientes feita, mas falta a lógica das opções e deixar mais bonita

--- src/app/Pages/UserPages/user-clients/user-clients.component.ts
+++ src/app/Pages/UserPages/user-clients/user-clients.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
 import { AvatarModule } from "primeng/avatar";
@@ -6,6 +6,10 @@ import { Ripple } from "primeng/ripple";
 import { InputTextModule } from "primeng/inputtext";
 import { InputIconModule } from 'primeng/inputicon';
 import { IconFieldModule } from 'primeng/iconfield';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+import { RatingModule } from 'primeng/rating';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-clients',
@@ -17,11 +21,41 @@ import { IconFieldModule } from 'primeng/iconfield';
     Ripple,
     InputTextModule,
     InputIconModule,
-    IconFieldModule
+    IconFieldModule,
+    TableModule,
+    RatingModule,
+    FormsModule
 ],
   templateUrl: './user-clients.component.html',
   styleUrl: './user-clients.component.css'
 })
 export class UserClientsComponent {
+  @ViewChild('dt') table:any;
+  cleamTable(){
+    this.table.reset();
+  }
+
+  clients:{name:string, cell:string, rating:number}[] = [
+    {name: 'oi', cell: 'oi', rating: 3},
+    {name: 'tachau', cell: 'oi', rating: 4},
+    {name: 'obrigado', cell: 'oi', rating: 3.5},
+    {name: 'feliz', cell: 'oi', rating: 5},
+    {name: 'natal', cell: 'oi', rating: 2},
+    {name: 'oi', cell: 'oi', rating: 3},
+    {name: 'tachau', cell: 'oi', rating: 4},
+    {name: 'obrigado', cell: 'oi', rating: 3.5},
+    {name: 'feliz', cell: 'oi', rating: 5},
+    {name: 'natal', cell: 'oi', rating: 2},
+    {name: 'oi', cell: 'oi', rating: 3},
+    {name: 'tachau', cell: 'oi', rating: 4},
+    {name: 'obrigado', cell: 'oi', rating: 3.5},
+    {name: 'feliz', cell: 'oi', rating: 5},
+    {name: 'natal', cell: 'oi', rating: 2},
+    {name: 'oi', cell: 'oi', rating: 3},
+    {name: 'tachau', cell: 'oi', rating: 4},
+    {name: 'obrigado', cell: 'oi', rating: 3.5},
+    {name: 'feliz', cell: 'oi', rating: 5},
+    {name: 'natal', cell: 'oi', rating: 2},
+  ]
 
 }
