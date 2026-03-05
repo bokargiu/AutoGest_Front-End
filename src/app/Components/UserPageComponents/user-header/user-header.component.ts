2de246e98d2e4a2cd707f34b333550a9068065b1
bokargiu <erickjstaack07@gmail.com>
2026-03-05 17:01:56 -0300

    sideBar funcionando corretamente no mobile

--- src/app/Components/UserPageComponents/user-header/user-header.component.ts
+++ src/app/Components/UserPageComponents/user-header/user-header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { AvatarModule } from 'primeng/avatar';
 import { Ripple } from "primeng/ripple";
-import { ButtonDirective } from "primeng/button";
 import { AuthService } from 'src/app/Services/AuthService/auth.service';
+import { SidebarModule } from 'primeng/sidebar';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SideMenuComponent } from "../side-menu/side-menu.component";
 
 @Component({
   selector: 'app-user-header',
@@ -10,13 +13,20 @@ import { AuthService } from 'src/app/Services/AuthService/auth.service';
   imports: [
     AvatarModule,
     Ripple,
+    SidebarModule,
+    SideMenuComponent
 ],
   templateUrl: './user-header.component.html',
   styleUrl: './user-header.component.css'
 })
 export class UserHeaderComponent {
   constructor(private _auth:AuthService){}
+  sidebarVisible: boolean = false;
+
   logout(){
     this._auth.Logout();
+  }
+  isMobile(){
+    return window.innerWidth < 800;
   }
 }
